c33d38fa7c880c6f56b90c10856ee8a7d30514f3
Gabriel Trombini <gabrieltrombini793@gmail.com>
2022-07-09 23:57:02 -0300

    add: validação

--- src/components/AdicionarUsuario/AdicionarUsuario.js
+++ src/components/AdicionarUsuario/AdicionarUsuario.js
@@ -1,76 +1,84 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Formik, useField } from 'formik';
+import * as yup from 'yup';
 
 import './AdicionarUsuario.css'
 
-function AdicionarUsuario() {
-  const [nome, setNome] = useState('')
-  const [sobrenome, setSobrenome] = useState('')
-  const [email, setEmail] = useState('')
 
-  const onSubmitHandler = event => {
-    event.preventDefault()
-    
-    const usuario = {nome,sobrenome,email}
+const Campo = ({label, ...props}) => {
+  const [field, meta] = useField(props);
+
+  return(
+    <div className="Coluna">
+      <label htmlFor={props.id}>{label}</label>
+      <input
+        {...field}
+        {...props}
+        
+      />
+      {meta.error && meta.touched ? (<div className="invalid-feedback">{meta.error}</div>) : null}
+
+    </div>
+  )
+}
+
+function AdicionarUsuario() {
+  
+  const onSubmitHandler = (values,{resetForm} ) => {
+    console.log(values)
     fetch('https://reqres.in/api/users', {
       method: 'POST',
-      body: JSON.stringify(usuario),
+      body: JSON.stringify(values),
       headers: {
         'Content-Type': 'application/json'
       }
     })
     .then(response => {
       if (response.ok) {
-        setNome('')
-        setSobrenome('')
-        setEmail('')
+        resetForm();
         alert('Usuário adicionado com sucesso!')
+        
       }
     })
     
   }
+  
+  const esquema = yup.object(
+    {
+      nome: yup.string()
+        .required('O nome é obrigatório')
+        .min(3, 'Nome muito curto')
+        .max(30, 'Nome muito longo'),
+      email: yup.string()
+        .required('O email é obrigatório')
+        .email('Email inválido'),
+      sobrenome: yup.string()
+        .required('O sobrenome é obrigatório')
+        .min(3, 'Sobrenome muito curto')
+        .max(30, 'Sobrenome muito longo'),
+    });
+
 
   return (
     <div className="AdicionarUsuario">
       <h2>Adicionar Usuário</h2>
-      <form onSubmit={onSubmitHandler}>
-        <div className="Linha">
-          <div className="Coluna">
-            <label>Nome</label>
-            <input
-              type="text"
-              name="nome"
-              value={nome}
-              onChange={event => setNome(event.target.value)}
-              required>
-            </input>
-          </div>
-          <div className="Coluna">
-            <label>Sobrenome</label>
-            <input
-              type="text"
-              name="sobrenome"
-              value={sobrenome}
-              onChange={event => setSobrenome(event.target.value)}
-              required>
-            </input>
-          </div>
-        </div>
-        <div className="Linha">
-          <div className="Coluna">
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-              required>
-            </input>
-          </div>
-        </div>
-        <button type="submit">
-          Adicionar
-      </button>
-      </form>
+      <Formik
+        initialValues={{nome:'', sobrenome:'', email:''}}
+        validationSchema={esquema}
+        onSubmit={onSubmitHandler}
+        >
+        {(props) => (
+          <form onSubmit={props.handleSubmit} noValidate>
+              <div className="Linha">
+                <Campo id="nome" name="nome" type="text" label="Nome"/>
+                <Campo id="sobrenome" name="sobrenome" type="text" label="Sobrenome" />
+              </div>
+              <div className="Linha">
+                <Campo id="email" name="email" type="email" label="Email"/>
+              </div>
+              <button type="submit">Adicionar</button>
+          </form>)}
+      </Formik>
     </div>
   )
   
